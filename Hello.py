8ebaa824908fc953eed07a5c52fa0486bc0060c7
leonardosaeta <58831999+leonardosaeta@users.noreply.github.com>
2021-09-30 00:08:03 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-'''
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -30,7 +29,7 @@ loss, accuracy = model.evaluate(x_test, y_test)
 
 print(loss)
 print(accuracy)
-'''
+
 model = tf.keras.models.load_model('handwritten.model')
 image_number = 0
 correct_prediction_check = 0
